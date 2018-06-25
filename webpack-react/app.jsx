9ab66256121f411510b2620e87a6734e94447f5f
Ilya <ilyakhd@users.noreply.github.com>
2018-06-25 19:19:07 +0300

    Add maps overview

--- webpack-react/app.jsx
+++ webpack-react/app.jsx
@@ -4,7 +4,7 @@ import * as ReactDOM from "react-dom";
 import "devextreme/dist/css/dx.common.css";
 import "devextreme/dist/css/dx.light.compact.css";
 
-import Overview from "./gauges-overview";
+import Overview from "./maps-overview";
 
 ReactDOM.render(
   <div>
