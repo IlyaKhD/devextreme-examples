05346c1d34259b88731920ca0a078a33f6ea92af
Ilya <ilyakhd@users.noreply.github.com>
2018-06-26 15:18:12 +0300

    Add dialogs overview

--- webpack-react/app.jsx
+++ webpack-react/app.jsx
@@ -4,7 +4,7 @@ import * as ReactDOM from "react-dom";
 import "devextreme/dist/css/dx.common.css";
 import "devextreme/dist/css/dx.light.compact.css";
 
-import Overview from "./maps-overview";
+import Overview from "./dialogs-and-notifications-overview";
 
 ReactDOM.render(
   <div>
